a35fed7a6ed072a2a2142d78be10d3e8126c63a7
tackhawk <tackhawk@gmail.com>
2013-11-29 14:50:38 -0800

    Initial Greed interface

--- src/game/Greed.java
+++ src/game/Greed.java
@@ -1,5 +1,18 @@
 package game;
 
-public interface Greed {
+import user.Player;
 
+public interface Greed {
+	
+	int getCurrentRoundNumber();
+	int getRoundsLeft();
+	
+	int getNumberOfPlayers();
+	int getPlayerPostion(Player p);
+	int getPlayerScore(Player p);
+	int[] getScoreboard();
+	
+	void addPlayerListener(Player p);
+	
+	int getDiceScore();
 }
